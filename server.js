470751872634956565c6251924eb6aefd4c42c23
Michelle Lavergne <michellelavergne.dev@gmail.com>
2021-03-24 14:53:42 -0400

    finished initial server

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
 
+app.use(cors())
+
+let characters = {
+
+    'darrow': {
+        'age' : 32,
+        'birthName' : 'Darrow O Lykos',
+        'birthLocation' : 'Mars',
+        'birthColor' : 'Red'
+    },
+    'sevro' : {
+        'age' : 32,
+        'birthName' : 'sevro au barca',
+        'birthLocation' : 'Mars',
+        'birthColor' : 'Gold'
+
+    },
+    'unknown' : {
+        'age' : 0,
+        'birthName' : 'unknown',
+        'birthLocation' : 'unknown',
+        'birthColor' : 'unknown'
+
+    }
+}
+
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html')  
+    response.sendFile(__dirname + '/index.html')  
+})
+
+app.get('/api/characters/:birthColor', (request, response) => {
+    const birthColor = request.params.birthColor.toLowerCase()
+    console.log(birthColor)
+    if(characters[birthColor]) {
+        response.json(characters[birthColor])
+    }else{
+        response.json(characters['unknown'])
+    }
+    
 })
 
 app.listen(PORT, () => {
-    console.log(`Sever running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
